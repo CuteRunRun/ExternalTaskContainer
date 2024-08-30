6b1cfff7ae75b29d7da46ebb95e4e726de2135c1
Peng Yan <gawaine1988@gmail.com>
2024-08-30 22:13:35 +0300

    init the project

---
+++ src/main/java/yp/externaltaskcontainer/ExternalTaskContainerApplication.java
@@ -0,0 +1,13 @@
+package yp.externaltaskcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalTaskContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalTaskContainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yp/externaltaskcontainer/ExternalTaskContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package yp.externaltaskcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalTaskContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
